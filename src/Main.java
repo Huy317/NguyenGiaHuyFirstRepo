35a8ce4e36a7fa2ae8d14ed38fbda4bf2c54efe9
Huy317 <>
2024-11-29 13:46:34 +0700

    Update print-name

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Welcome to Nguyen_Gia_Huy's Java Repository");
+        System.out.println("Hello Nguyen Gia Huy.");
+        System.out.println("Welcome to my repository.");
     }
 }
