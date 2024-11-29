7f2bc2a3a9a0cf94096e675cdda9b916f9a77930
Huy317 <>
2024-11-29 13:51:22 +0700

    update input name

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class Main{
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello Nguyen Gia Huy.");
+        String name = sc.nextLine();
+        System.out.println("Hello "+name+",");
         System.out.println("Welcome to my repository.");
     }
 }
